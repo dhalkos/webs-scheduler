82daf7126697e94a19b46c9c8f59817ef99985a9
Dimitris Chalkos <dhalkos@gmail.com>
2018-05-02 13:52:50 +0300

    Add application configuration code

--- src/main/java/gr/dhalk/scheduler/config/Application.java
+++ src/main/java/gr/dhalk/scheduler/config/Application.java
@@ -10,11 +10,14 @@ import javax.servlet.ServletException;
 import org.ocpsoft.rewrite.servlet.RewriteFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
@@ -23,10 +26,11 @@ import org.springframework.scheduling.annotation.EnableScheduling;
  */
 
 
+@Configuration
 @EnableAutoConfiguration
 @EnableScheduling
 @ComponentScan({"gr.dhalk.scheduler"})
-public class Application implements ServletContextInitializer {
+public class Application extends SpringBootServletInitializer implements ServletContextInitializer  {
 
 	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -59,5 +63,15 @@ public class Application implements ServletContextInitializer {
 		
 	}
 
-    
+	/* (non-Javadoc)
+	 * @see org.springframework.boot.web.servlet.support.SpringBootServletInitializer#configure(org.springframework.boot.builder.SpringApplicationBuilder)
+	 */
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+	
+		return builder.sources(
+                new Class[] { Application.class});
+	}
+
+	
 }
